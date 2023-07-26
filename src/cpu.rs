0f18e4a1e0d5bc8ec9333b80a88861c043932837
Ermaolaoye(Zihang Huang) <56563171+Ermaolaoye233@users.noreply.github.com>
2023-07-26 21:52:01 +0100

    implemented addressing mode

--- src/cpu.rs
+++ src/cpu.rs
@@ -92,5 +92,151 @@ impl CPU {
 
         self.cycles -= 1;
     }
-   
+}
+
+impl CPU {
+    // Addressing Modes
+    // Returned integer is the additional number of cycles required for the Instruction
+
+    fn imp(&mut self) -> u8{
+        // Some instructions use the accumulator's value as operand
+        self.fetched = self.accumulator;
+        0
+    }
+
+    fn imm(&mut self) -> u8 {
+        self.addr_abs = self.program_counter;
+        self.program_counter += 1;
+        0
+    }
+
+    fn zp0(&mut self) -> u8 {
+        self.addr_abs = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+        self.addr_abs &= 0x00FF;
+        0
+    }
+
+    fn zpx(&mut self) -> u8 {
+        self.addr_abs = (self.read(self.program_counter, false) + self.x_register) as u16;
+        self.program_counter += 1;
+        self.addr_abs &= 0x00FF;
+        0
+    }
+
+    fn zpy(&mut self) -> u8 {
+        self.addr_abs = (self.read(self.program_counter, false) + self.y_register) as u16;
+        self.program_counter += 1;
+        self.addr_abs &= 0x00FF;
+        0
+    }
+
+    fn abs(&mut self) -> u8 {
+        //6502 stores memory address in little endian format
+        let lo = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let hi = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        self.addr_abs = (hi << 8) | lo;
+        0
+    }
+
+    fn abx(&mut self) -> u8 {
+        let lo = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let hi = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        self.addr_abs = (hi << 8) | lo;
+        self.addr_abs += self.x_register as u16;
+
+        // If the addition of the offset causes a change in the high byte, an additional cycle is required
+        if (self.addr_abs & 0xFF00) != (hi << 8) {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn aby(&mut self) -> u8 {
+        let lo = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let hi = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        self.addr_abs = (hi << 8) | lo;
+        self.addr_abs += self.y_register as u16;
+
+        // If the addition of the offset causes a change in the high byte, an additional cycle is required
+        if (self.addr_abs & 0xFF00) != (hi << 8) {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn ind(&mut self) -> u8 {
+        let ptr_lo = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let ptr_hi = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let ptr = (ptr_hi << 8) | ptr_lo;
+
+        // 6502 bug where if the low byte of the supplied address is 0xFF, the high byte is fetched from the low byte of the supplied address
+        // This is added for bug for bug compatibility
+        if ptr_lo == 0x00FF {
+            self.addr_abs = (self.read(ptr & 0xFF00, false) as u16) << 8 | self.read(ptr, false) as u16;
+        } else {
+            self.addr_abs = (self.read(ptr + 1, false) as u16) << 8 | self.read(ptr, false) as u16;
+        }
+
+        0
+    }
+
+    fn izx(&mut self) -> u8 {
+        let t = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let lo = self.read((t + self.x_register as u16) & 0x00FF, false) as u16;
+        let hi = self.read((t + self.x_register as u16 + 1) & 0x00FF, false) as u16;
+
+        self.addr_abs = (hi << 8) | lo;
+        0
+    }
+
+    fn izy(&mut self) -> u8 {
+        let t = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        let lo = self.read(t & 0x00FF, false) as u16;
+        let hi = self.read((t + 1) & 0x00FF, false) as u16;
+
+        self.addr_abs = (hi << 8) | lo;
+        self.addr_abs += self.y_register as u16;
+
+        // If the addition of the offset causes a change in the high byte, an additional cycle is required
+        if (self.addr_abs & 0xFF00) != (hi << 8) {
+            1
+        } else {
+            0
+        }
+    }
+
+    fn rel(&mut self) -> u8 {
+        self.addr_rel = self.read(self.program_counter, false) as u16;
+        self.program_counter += 1;
+
+        // two's complement to convert to signed integer
+        if (self.addr_rel & 0x80) != 0 {
+            self.addr_rel |= 0xFF00;
+        }
+
+        0
+    }
 }
